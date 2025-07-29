556c3bd853207179379fff11ffb6a9220bf132dd
Rindra Hosea Mahefaniaina <mahefaniainarindra@gmail.com>
2025-07-29 18:36:49 +0300

    transfer

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from account.views import AddBeneficiaryView, BeneficiaryView
+from account.views import AddBeneficiaryView, BeneficiaryView, TransferView
 
 urlpatterns = [
     path("beneficiary/list", BeneficiaryView.as_view(), name='list-beneficiary'),
-    path("beneficiary/add", AddBeneficiaryView.as_view(), name='add-beneficiary')
+    path("beneficiary/add", AddBeneficiaryView.as_view(), name='add-beneficiary'),
+    path("tranfer/", TransferView.as_view(), name='tranfer')
 ]
